cfb4b42c782a0039f64877167d955cdde8863513
pbialecki_augeofi <pbialecki@augeofi.com>
2020-09-30 21:00:31 -0500

    Additonal table styling.

--- website/four-in-a-row/js/app.js
+++ website/four-in-a-row/js/app.js
@@ -5,6 +5,7 @@ document.getElementById('begin-game').addEventListener('click', function () {
     this.style.display = 'none';
     document.getElementById('play-area').style.opacity = '1';
     document.getElementById('game-scene').style.display = 'block';
+    document.getElementById('table').style.display = 'block';
 });
 
 document.addEventListener('keydown', function (event) {
